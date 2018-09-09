4c01d65a394c2836e7db25b5783360cf6cd5b029
Noora Mäkelä <noora.m.makela@helsinki.fi>
2018-09-09 20:26:57 +0300

    Done till exercise 6

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,7 @@
 class RatingsController < ApplicationController
+    #before_action :set_brewery, only: [:show, :edit, :update, :destroy]
+
     def index
+        @ratings = Rating.all
     end
   end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,9 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        mean = ratings.map(&:score).inject(0, &:+)
+        mean/ratings.count
+    end
 end
