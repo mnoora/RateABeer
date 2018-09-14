a8a35c544c96fde82bb96cfae6b316278a02b910
Noora Mäkelä <noora.m.makela@helsinki.fi>
2018-09-14 12:46:34 +0300

    Kakkosviikon tehtävä 13 asti

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,22 @@
 class RatingsController < ApplicationController
     #before_action :set_brewery, only: [:show, :edit, :update, :destroy]
 
+    def create
+        Rating.create params.require(:rating).permit(:score, :beer_id)
+        redirect_to ratings_path
+    end
     def index
         @ratings = Rating.all
+    end
+
+    def new
+        @rating = Rating.new
+        @beers = Beer.all
+    end
+
+    def destroy
+        rating = Rating.find(params[:id])
+        rating.delete
+        redirect_to ratings_path
     end
   end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,9 +1,13 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
-    has_many :ratings
+    has_many :ratings, dependent: :destroy
 
     def average_rating
         mean = ratings.map(&:score).inject(0, &:+)
         mean/ratings.count
+    end
+
+    def to_s
+        "#{name} #{brewery.name}"
     end
 end
